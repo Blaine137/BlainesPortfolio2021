c01c498beec9a52e5f5eb6879c90032ba814052b
Blaine Young <blaine.young3553@gmail.com>
2021-10-04 20:50:19 -0400

    finishing touches on Home section and starts about section

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 let menuButton = document.querySelector('.hamburger');
 let sideMenu = document.querySelector('.sideMenu');
+let links = document.querySelectorAll('.navLink');
 
 //CLICK EVENT FOR MOBILE SHOW/HIDE NAV
 menuButton.addEventListener('click', () => {
@@ -11,3 +12,11 @@ menuButton.addEventListener('click', () => {
         sideMenu.classList.add('sideMenuHide');
     }
 });
+
+//CLICK EVENT TO CLOSE THE NAVIGATION ON CLICK OF A LINK
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        sideMenu.classList.remove('sideMenuShow');
+        sideMenu.classList.add('sideMenuHide');
+    });
+})
